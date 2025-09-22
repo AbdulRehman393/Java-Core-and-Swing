b2665e4ce5a4bce322cca609c32e8c6fdaacc9ff
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-09-22 14:31:49 +0500

    Add files via upload

---
+++ JavaGUI/src/newpackage/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+/**
+ *
+ * @author ABDUL REHMAN
+ */
+public class Main {
+    public static void main(String[] args) {
+       /* JFrame frame=new JFrame();
+        frame.setTitle("GUI JFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        //frame.setDefaultCloseOperation(JFrame.DO_Nothing_ON_CLOSE);
+        //frame.setResizable(false);
+        frame.setSize(400,400);
+        frame.setVisible(true);
+        
+        
+        ImageIcon image=new ImageIcon("logo2.png");
+       frame.setIconImage(image.getImage());
+       frame.getContentPane().setBackground(new Color(0,0,255));
+      
+*/
+MyFrame myFrame=new MyFrame();
+
+
+    }
+    
+}
