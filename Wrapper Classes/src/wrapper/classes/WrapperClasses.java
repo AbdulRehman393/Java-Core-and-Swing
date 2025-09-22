450567f14fee0b8b9b1cb93467576799dcac3467
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-09-22 13:52:31 +0500

    Add files via upload

---
+++ Wrapper Classes/src/wrapper/classes/WrapperClasses.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wrapper.classes;
+
+/**
+ *
+ * @author ABDUL REHMAN
+ */
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+   Boolean a= true;
+   Character b='a';
+   Integer c=123;
+   Double d=232.231;
+   String e="Hello";
+   Byte f=120;
+   Float g=23.42f;
+   Short i=1233;
+   Long j=322432L;
+   char x= e.charAt(0);
+        System.out.println(x);   //Output 6
+        
+     if(a==true)
+     {
+         System.out.println("This is true.");
+     }
+        
+    }
+    
+}
